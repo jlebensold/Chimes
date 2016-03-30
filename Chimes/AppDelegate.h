a735a86520663b33b9d7b4e72f33774e7ff2d952
Jon Lebensold <jon@lebensold.ca>
2016-03-30 21:57:55 +0200

    Initial Commit

---
+++ Chimes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chimes
+//
+//  Created by Jonathan Lebensold on 3/30/16.
+//  Copyright © 2016 Jonathan Lebensold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chimes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chimes
+//
+//  Created by Jonathan Lebensold on 3/30/16.
+//  Copyright © 2016 Jonathan Lebensold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
